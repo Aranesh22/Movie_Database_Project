b4c801bee5db798c03323061beb0f36dbe128119
kevin zhang <kcrafterzh01@gmail.com>
2021-03-05 16:31:25 -0500

    Update MovieServer.js

--- server/MovieServer.js
+++ server/MovieServer.js
@@ -1,3 +1,4 @@
+/*
 const http = require("http");
 const pug = require("pug");
 const fs = require("fs");
@@ -22,3 +23,35 @@ const server = http.createServer(function(request, response){
 
 server.listen(3000);
 console.log("http://localhost:3000/");
+*/
+
+const express = require("express");
+const app = express();
+
+app.set("View engine", "pug");
+app.set("views", "../Pages");
+
+app.get("/", (req, res, next) => {res.render("index.pug");});
+
+let usersRouter = require("./routers/users-router");
+app.set("/users", usersRouter);
+
+app.get("/profile", (req, res, next) =>{
+    let profile = {
+        "id":1, 
+        "username":"bumbleboy", 
+        "password":"scoopme", 
+        "contributingAccount":false, 
+        "followingPeople":[], 
+        "followingUsers":[], 
+        "followers":[],
+        "watchList":[],
+        "viewRecMovies":[],
+        "userNotifications":[],
+        "userReviews":[]
+    }
+    res.status(200).render("profile.pug", {profile: profile});
+});
+
+app.listen(3000);
+console.log("Server listening at http://localhost:3000");
