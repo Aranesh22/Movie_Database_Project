b093b4e661b1d86748a8ed7b6fc875a478bc7006
kevin zhang <kcrafterzh01@gmail.com>
2021-03-05 20:28:53 -0500

    Update users-router.js

--- server/routers/users-router.js
+++ server/routers/users-router.js
@@ -59,7 +59,7 @@ function queryParser(req, res, next){
 }
 
 function checkName(user, query){
-    return !query.name || user.name.toLowerCase().includes(query.name.toLowerCase())
+    return !query.username || user.username.toLowerCase().includes(query.username.toLowerCase())
 }
 
 function loadUsers(req, res, next){
@@ -94,7 +94,7 @@ function loadUsers(req, res, next){
 
 function sendUsers(req, res, next){
     res.format({
-        "text/html": () => {res.status(200).render("../../Pages/users.pug", {users:res.users})},
+        "text/html": () => {res.status(200).render("users.pug", {users:res.users, qstring:res.qstring, current:res.query.page})},
         "application/json": () => {res.status(200).json(res.users)}
     });
     next();
@@ -120,7 +120,7 @@ function getUser(req, res, next){
 
 function sendUser(req, res, next){
     res.format({
-        "text/html": () => {res.status(200).render("users.pug", {users:res.user})},
+        "text/html": () => {res.status(200).render("user.pug", {users:res.user})},
         "application/json": () => {res.status(200).json(res.user)}
     });
     next();
