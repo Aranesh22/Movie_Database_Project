cb0ea462293560545c5443c836158c5a251eeb02
Aranesh22 <a.aranesh@gmail.com>
2021-03-13 19:12:09 -0500

    Update MovieServer.js

--- server/MovieServer.js
+++ server/MovieServer.js
@@ -29,38 +29,38 @@ const express = require("express");
 const app = express();
 
 app.set("View engine", "pug");
-app.set("views", "../Pages");
+app.set("views", "../Pages"); 
 
 app.get("/", (req, res, next) => {res.render("index.pug");});
 
 //users
 let usersRouter = require("./routers/users-router");
-app.set("/users", usersRouter);
+app.use("/users", usersRouter);
 
 //movies
 let movierouter = require("./routers/movies-router");
-app.set("/movies", movierouter);
+app.use("/movies", movierouter);
 
 //people
 let peopleRouter = require("./routers/people-router");
-app.set("/people", peopleRouter);
+app.use("/people", peopleRouter);
 
 //profile
 app.get("/profile", (req, res, next) =>{
     let profile = {
         "id":1, 
-        "username":"bumbleboy", 
-        "password":"scoopme", 
-        "contributingAccount":false, 
-        "followingPeople":[], 
-        "followingUsers":[], 
-        "followers":[],
-        "watchList":[],
-        "viewRecMovies":[],
-        "userNotifications":[],
-        "userReviews":[]
+        username:"bumbleboy", 
+        password:"scoopme", 
+        contributingAccount:false, 
+        followingPeople:[], 
+        followingUsers:[], 
+        followers:[],
+        watchList:[],
+        viewRecMovies:[],
+        userNotifications:[],
+        userReviews:[]
     }
-    res.status(200).render("profile.pug", {profile: profile});
+    res.status(200).render("profile.pug", {profile: profile}); 
 });
 
 //add function
@@ -74,7 +74,7 @@ app.get("/addPerson", (req, res, next) => {
 
 app.get("/createAccount", (req, res, next) => {
     res.status(200).render("createAccount.pug");
-});
+}); 
 
 app.listen(3000);
 console.log("Server listening at http://localhost:3000");
