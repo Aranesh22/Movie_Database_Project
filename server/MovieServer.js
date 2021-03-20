d67a7a16f7b3796e770178b202a844d1c25a0249
kevin zhang <kcrafterzh01@gmail.com>
2021-03-20 12:35:28 -0400

    Update MovieServer.js

--- server/MovieServer.js
+++ server/MovieServer.js
@@ -1,30 +1,3 @@
-/*
-const http = require("http");
-const pug = require("pug");
-const fs = require("fs");
-
-const server = http.createServer(function(request, response){
-    if(request.method === "GET"){
-        if(request.url === "/" || request.url === "/index.html"){
-            let data = pug.renderFile("Pages/index.pug");
-            response.statusCode = 200;
-            response.end(data);
-            return;
-        } 
-        else if(request.url === "/profile"){
-            let data = pug.renderFile("Pages/profile.pug");
-            response.statusCode = 200;
-            response.end(data);
-            return;
-        }
-    } 
-
-});
-
-server.listen(3000);
-console.log("http://localhost:3000/");
-*/
-
 const express = require("express");
 const app = express();
 
