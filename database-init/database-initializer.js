15528cf25cd1c600dcda0e2f02e896df60f9a902
kevin zhang <kcrafterzh01@gmail.com>
2021-03-28 19:37:11 -0400

    Update database-initializer.js

--- database-init/database-initializer.js
+++ database-init/database-initializer.js
@@ -1,1 +1,19 @@
 const mongoose = require("mongoose");
+const Movie = require("./movieModel");
+const People = require("./peopleModel");
+
+mongoose.connect("mongodb://localhost/final", {useNewUrlParser: true});
+let db = mongoose.connection;
+
+db.on("error", consol.error.bind(console, "connection error"));
+db.once("open", function(){
+    mongoose.connection.db.dropDatabase(function(err, result){
+        if(err){
+            console.log("error dropping database:");
+            console.log("error");
+            return;
+        }
+        console.log("Dropped final database");
+        
+    });
+});
