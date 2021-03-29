c0333157056c664fd6b02f999a06dccd51214c2c
kevin zhang <kcrafterzh01@gmail.com>
2021-03-29 16:01:39 -0400

    Update peopleModel.js

--- database-init/peopleModel.js
+++ database-init/peopleModel.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let peopleSchema = Schema({
-    name: String,
+    name: {type: String, unique: true},
     directed: {type: [Schema.Types.ObjectId], ref: "Movie"},
     written: {type: [Schema.Types.ObjectId], ref: "Movie"},
     acted: {type: [Schema.Types.ObjectId], ref: "Movie"},
     freqCollaborators: {type: [Schema.Types.ObjectId], ref: "People"},
-    followers: [Schema.Types.ObjectId]
+    followers: {type: [Schema.Types.ObjectId], ref: "User"}
 });
 
 module.exports = mongoose.model("People", peopleSchema);
