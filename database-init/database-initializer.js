5e140787a9e41825a8657e36990009f98ac4b852
kevin zhang <kcrafterzh01@gmail.com>
2021-03-28 21:00:44 -0400

    Update database-initializer.js

--- database-init/database-initializer.js
+++ database-init/database-initializer.js
@@ -5,7 +5,7 @@ const People = require("./peopleModel");
 mongoose.connect("mongodb://localhost/final", {useNewUrlParser: true});
 let db = mongoose.connection;
 
-db.on("error", consol.error.bind(console, "connection error"));
+db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function(){
     mongoose.connection.db.dropDatabase(function(err, result){
         if(err){
@@ -14,6 +14,27 @@ db.once("open", function(){
             return;
         }
         console.log("Dropped final database");
-        
+        let movies = require("../Json/movie-data-10.json");
+        movies.forEach((movie) => {
+            let temp = {};
+            temp.title = movie.Title;
+            temp.year = movie.Year;
+            temp.rating = movie.Rated;
+            temp.released = movie.Released;
+            temp.runtime = movie.Runtime;
+            temp.genre = movie.Genre;
+            temp.director = [];
+            temp.writer = [];
+            temp.actors = [];
+            temp.plot = movie.Plot;
+            temp.awards = movie.Awards;
+            temp.poster = movie.Poster;
+            temp.reviews = [];
+            temp.similarMovies = [];
+            let m = new Movie(temp);
+            m.save(function(err, callback){
+                if(err) console.log(err.message);
+            });
+        });
     });
 });
