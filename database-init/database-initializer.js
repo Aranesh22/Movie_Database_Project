1800a6a5e0b894ab9c44ab2898ed5e0a69cf5982
kevin zhang <kcrafterzh01@gmail.com>
2021-03-29 16:01:36 -0400

    Update database-initializer.js

--- database-init/database-initializer.js
+++ database-init/database-initializer.js
@@ -14,27 +14,86 @@ db.once("open", function(){
             return;
         }
         console.log("Dropped final database");
-        let movies = require("../Json/movie-data-10.json");
+
+        let people = []; //prevent duplicates
+        let p; //p model
+
+        //init movies and people
+        let movies = require("../Json/movie-data-100.json");
+
         movies.forEach((movie) => {
-            let temp = {};
-            temp.title = movie.Title;
-            temp.year = movie.Year;
-            temp.rating = movie.Rated;
-            temp.released = movie.Released;
-            temp.runtime = movie.Runtime;
-            temp.genre = movie.Genre;
-            temp.director = [];
-            temp.writer = [];
-            temp.actors = [];
-            temp.plot = movie.Plot;
-            temp.awards = movie.Awards;
-            temp.poster = movie.Poster;
-            temp.reviews = [];
-            temp.similarMovies = [];
-            let m = new Movie(temp);
+
+            //add movies to db
+            let tempMovie = {};
+            tempMovie.title = movie.Title;
+            tempMovie.year = movie.Year;
+            tempMovie.rating = movie.Rated;
+            tempMovie.released = movie.Released;
+            tempMovie.runtime = movie.Runtime;
+            tempMovie.genre = movie.Genre;
+            tempMovie.director = [];
+            tempMovie.writer = [];
+            tempMovie.actors = [];
+            tempMovie.plot = movie.Plot;
+            tempMovie.awards = movie.Awards;
+            tempMovie.poster = movie.Poster;
+            tempMovie.reviews = [];
+            tempMovie.similarMovies = [];
+            let m = new Movie(tempMovie);
             m.save(function(err, callback){
                 if(err) console.log(err.message);
             });
+
+            //add people to db
+            movie.Director.forEach((dir) => {
+                if(!people.includes(dir)){
+                    p = new People({
+                        "name":dir,
+                        "directed":[],
+                        "acted":[],
+                        "written":[],
+                        "freqCollab":[],
+                        "followers":[]
+                    });
+                    p.save(function(err, callback){
+                        if (err) console.log(err.message);
+                    });
+                    people.push(dir);
+                }
+            });
+            movie.Writer.forEach((write) => {
+                if(!people.includes(write)){
+                    p = new People({
+                        "name":write,
+                        "directed":[],
+                        "acted":[],
+                        "written":[],
+                        "freqCollab":[],
+                        "followers":[]
+                    });
+                    p.save(function(err, callback){
+                        if (err) console.log(err.message);
+                    });
+                    people.push(write);
+                }
+            });
+            movie.Actors.forEach((act) => {
+                if(!people.includes(act)){
+                    p = new People({
+                        "name":act,
+                        "directed":[],
+                        "acted":[],
+                        "written":[],
+                        "freqCollab":[],
+                        "followers":[]
+                    });
+                    p.save(function(err, callback){
+                        if (err) console.log(err.message);
+                    });
+                    people.push(act);
+                }
+            });
+            
         });
     });
 });
