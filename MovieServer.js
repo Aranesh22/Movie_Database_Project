a64ff6b57cc06f8ef32f7cf767d71b4f95f468a2
kevin zhang <kcrafterzh01@gmail.com>
2021-04-01 16:13:48 -0400

    using cookie dat for rendering

--- MovieServer.js
+++ MovieServer.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
@@ -8,11 +9,25 @@ app.use(function(req, res, next){
     next();
 });
 
+mongoose.connect("mongodb://localhost/final", {useNewUrlParser: true});
+let db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error"));
+
 app.use(express.static("public"));
 app.set("View engine", "pug");
 app.set("views", "Pages"); 
 
-app.get("/", (req, res) => {res.render("index.pug");});
+app.get("/", (req, res) => {
+    let loggedin;
+    if(!req.session.loggedin){
+        loggedin = false;
+    }
+    else{
+        loggedin = true;
+    }
+    res.render("index.pug", {loggedin: loggedin});
+});
 
 //users
 let usersRouter = require("./routers/users-router");
