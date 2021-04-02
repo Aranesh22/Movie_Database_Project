e0f2078969400bdb526c88c51812994411e6a42f
kevin zhang <kcrafterzh01@gmail.com>
2021-04-02 12:26:47 -0400

    added db functionality

--- routers/users-router.js
+++ routers/users-router.js
@@ -1,11 +1,13 @@
 const express = require("express");
 let router = express.Router();
+const Movie = require("../database-init/movieModel");
+const People = require("../database-init/personModel");
+const User = require("../database-init/userModel");
 
-router.get("/", queryParser);
-router.get("/", loadUsers);
-router.get("/", sendUsers);
+router.get("/", queryParser, loadUsers, sendUsers);
 
-router.get("/:uid", getUser, sendUser);
+router.get("/:uID", getUser, sendUser);
+
 function queryParser(req, res, next){
     const MAX_USERS = 50;
 
@@ -57,77 +59,72 @@ function queryParser(req, res, next){
 	next();
 }
 
-function checkName(user, query){
-    return !query.username || user.username.toLowerCase().includes(query.username.toLowerCase());
-}
-
 function loadUsers(req, res, next){
-    let results = [];
-    let users = [{
-        "id":1, 
-        username:"bumbleboy", 
-        password:"scoopme", 
-        contributingAccount:false, 
-        followingPeople:[], 
-        followingUsers:[], 
-        followers:[],
-        watchList:[],
-        viewRecMovies:[],
-        userNotifications:[],
-        userReviews:[]
-    }]
-
-    let count = 0;
-    for(let i = 0; i < users.length; i++){
-        if(checkName(users[i], req.query)){
-            results.push(users[i]);
-            count++;
+    let startIndex = (req.query.page - 1) * req.query.limit;
+    let amount = req.query.limit;
+    
+    User.find().byName(req.query.username).limit(amount).skip(startIndex).exec(function(err, result){
+        if(err){
+            res.status(500).send("Database error");
+            console.log(err.message);
+            return;
         }
-        if(count == req.limit){
-            break;
-        }
-    }
-    res.users = results;
-    next();
+        res.users = result;
+        next();
+        return;
+    });
 }
 
 function sendUsers(req, res, next){
+    console.log(res.users);
     res.format({
-        "text/html": () => {res.status(200).render("users.pug", {users:res.users, qstring:req.qstring, current:req.query.page})},
+        "text/html": () => {res.status(200).render("users.pug", {
+            users:res.users, 
+            qstring:req.qstring, 
+            current:req.query.page
+        })},
         "application/json": () => {res.status(200).json(res.users)}
     });
     next();
 }
 
 function getUser(req, res, next){
-    let user = {
-        id:1, 
-        username:"bumbleboy", 
-        password:"scoopme", 
-        contributingAccount:false, 
-        followingPeople: {followingPpl:["JackEvans","Todd","Benny","Link"]}, 
-        followingUsers:{followingUser:["Jimmy","Tony Stark","Bat Man"]}, 
-        followers:{ followers:["Brad","Steve","Ogeyyy"]},
-        viewRecMovies: [
-            {title: "Jack and Jill", id:"678"}, 
-            {title: "Power Rangers", id:"020"}, 
-            {title: "Super Kevin", id:"999"}
-            ],
-        watchList: [
-            {title: "Jack and Jill", id:"678"}, 
-            {title: "Power Rangers", id:"020"}, 
-            {title: "Super Kevin", id:"999"}
-            ],
-        userNotifications:{ userNot:["Jack Upploaded Movie","Tod is acting a new film"]},
-        userReviews:{uReviews:["Hello Was A Good Movie","Its me movie could be better"]},
-    }
-    res.user = user
+    let id = req.params.uID;
+    User.findById(id, function(err, user){ //find user
+        if(err){            
+            res.status(500).send("Database error");
+            console.log(err.message);
+            return;
+        }
+        People.find({_id: {$in: user.followingPeople}}, function(err, people){ //find people user follows
+            if(err){            
+                res.status(500).send("Database error");
+                console.log(err.message);
+                return;
+            }
+            Movie.find({_id: {$in: user.watchedList}}, function(err, movies){ //find movies in users watched list
+                if(err){            
+                    res.status(500).send("Database error");
+                    console.log(err.message);
+                    return;
+                }
+                res.user = user;
+                res.people = people;
+                res.movies = movies;
+                next()
+            });
+        });
+    });
     next();
 }
 
 function sendUser(req, res, next){
     res.format({
-        "text/html": () => {res.status(200).render("user.pug", {user:res.user})},
+        "text/html": () => {res.status(200).render("user.pug", {
+            user:res.user,
+            people: res.people,
+            watchedMovies: res.movies,
+        })},
         "application/json": () => {res.status(200).json(res.user)}, 
     }); 
    
