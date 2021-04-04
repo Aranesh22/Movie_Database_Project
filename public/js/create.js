f03161eac70ce35a73bbcd3c50b1f888ca9d0a6d
kevin zhang <kcrafterzh01@gmail.com>
2021-04-04 19:05:45 -0400

    Update create.js

--- public/js/create.js
+++ public/js/create.js
@@ -4,11 +4,20 @@ function createAccount(){
 
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
+
+    if(username == "" || password == ""){
+        alert("Both fields must be filled");
+        return;
+    }
+
     let req = new XMLHttpRequest()
     req.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 201){
             console.log(this.responseText);
             window.location.replace("http://localhost:3000/login");
+        }
+        if(this.readyState == 4 && this.status == 403){
+            alert("Username unavailable");
         }
     }
     req.open("POST", "http://localhost:3000/users");
