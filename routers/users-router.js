48fc42e0965ca7c8d038ce6b37f95f0fa97c6f9a
kevin zhang <kcrafterzh01@gmail.com>
2021-04-04 12:58:30 -0400

    Update users-router.js

--- routers/users-router.js
+++ routers/users-router.js
@@ -9,12 +9,8 @@ router.post("/", express.json(), createUser);
 router.get("/:uID", getUser, sendUser);
 router.put("/:uID/follow", follow);
 router.put("/:uID/unfollow", unfollow);
-router.put("/accountType", toggle);
-router.get("/profile", getProfile, sendProfile);
 router.get("/:uID/userFollowing", getUserFollowers, sendUserFollowers);
 router.get("/:uID/peopleFollowing", getPeopleFollowers, sendPeopleFollowers);
-router.put("/login", express.json(), login);
-router.put("/logout", logout);
 
 function queryParser(req, res, next){
     const MAX_USERS = 50;
@@ -268,110 +264,6 @@ function unfollow(req, res, next){
     });
 }
 
-function toggle(req, res, next){
-    if(!req.session.loggedin){
-        res.status(401).send("Not logged in");
-        return;
-    }
-    User.findById(req.session._id, function(err, result){
-        if(err){
-            console.log(err.message);
-            res.status(500).send("Database error");
-            return; 
-        }
-        if(result.contributingAccount){
-            result.contributingAccount = false;
-        }
-        else{
-            result.contributingAccount = true;
-        }
-        result.save(function(err){
-            if(err){
-                console.log(err.message);
-                res.status(500).send("Database error");
-                return; 
-            }
-            next();
-        });
-    });
-}
-
-function getProfile(req, res, next){
-    if(!req.session.loggedin){
-        res.status(401).send("Not logged in");
-        return;
-    }
-    User.findById(req.session._id, function(err, user){
-        if(err){
-            console.log(err.message);
-            res.status(500).send("Database error");
-            return;
-        }
-        if(!user){
-            res.status(404).send('User not found');
-            return;
-        }
-        People.find({_id: {$in: user.followingPeople}}, function(err, people){
-            if(err){
-                console.log(err.message);
-                res.status(500).send("Database error");
-                return;
-            }
-            User.find({_id: {$in: user.followingUsers}}, function(err, users){
-                if(err){
-                    console.log(err.message);
-                    res.status(500).send("Database error");
-                    return;
-                }
-                Movie.find({_id: {$in: user.watchList}}, function(err, watchedMovies){
-                    if(err){
-                        console.log(err.message);
-                        res.status(500).send("Database error");
-                        return;
-                    }
-                    Movies.find({_id: {$in: user.recMovies}}, function(err, recMovies){
-                        if(err){
-                            console.log(err.message);
-                            res.status(500).send("Database error");
-                            return;
-                        }
-                        res.user = user;
-                        res.people = people;
-                        res.users = users;
-                        res.watchedMovies = watchedMovies;
-                        res.recMovies = recMovies;
-                        next();
-                    });
-                });
-            });
-        });
-    });
-}
-
-function sendProfile(req, res, next){
-    res.format({
-        "text/html" : () => {
-            res.status(200).render("profile.pug", {
-                user: res.user,
-                people: res.people,
-                users: res.users,
-                watchedMovies: res.watchedMovies,
-                recMovies: res.recMovies
-            });
-        },
-        "application/json": () => {
-            let profile = {};
-            profile.user = res.user;
-            profile.people = res.people;
-            profile.users = res.users;
-            profile.watchedMovies = res.watchedMovies;
-            profile.recMovies = res.recMovies;
-            res.status(200).json(profile);
-        }
-    });
-    next();
-}
-
 function getUserFollowers(req, res, next){
     let id = req.params.uID;
     User.findById(id, function(err, user){
@@ -426,52 +318,6 @@ function getPeopleFollowers(req, res, next){
 function sendPeopleFollowers(req, res, next){
     res.status(200).send(res.people);
     next();
-}
-
-function login(req, res, next){
-    if(req.session.loggedin){
-        res.status(401).send("Already logged in");
-    }
-    User.findOne({username: req.body.username}, function(err, result){
-        if(err){
-            console.log(err.message);
-            res.status(500).send("Database error");
-            return;
-        }
-        if(!result){
-            console.log("No user");
-            res.status(401).send("User does not exist");
-            return;
-        }
-        console.log(result);
-        console.log(result.password + " " + req.body.password);
-        if(result.password == req.body.password){
-            console.log("loggedin");
-            req.session.loggedin = true;
-            req.session.username = result.username;
-            req.session._id = result._id;
-            req.session.contributingAccount = result.contributingAccount;
-            res.status(204).send("Successful login");
-        }
-        else{
-            console.log("incorrect password");
-            res.status(401).send("Incorrect password");
-        }
-        next();
-    });
-}
-
-function logout(req, res, next){
-    if(req.session.loggedin){
-        req.session.loggedin = false;
-        req.session.username = undefined;
-        req.session._id = undefined;
-        req.session.contributingAccount = false;
-    }
-    else{
-        res.status(401).send("Not loggedin");
-    }
-    res.status(204).send("Successful logout");
 }
 
 module.exports = router;
