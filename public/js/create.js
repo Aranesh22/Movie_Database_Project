10e7afa1cf14799e68eb23b1dad76ba25e5f0d16
kevin zhang <kcrafterzh01@gmail.com>
2021-04-04 11:57:41 -0400

    Update create.js

--- public/js/create.js
+++ public/js/create.js
@@ -8,7 +8,7 @@ function createAccount(){
     req.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 201){
             console.log(this.responseText);
-            window.location.replace("http://localhost:3000/users/login");
+            window.location.replace("http://localhost:3000/login");
         }
     }
     req.open("POST", "http://localhost:3000/users");
