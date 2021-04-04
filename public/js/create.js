bdc33070eba3ebd93ffee355ca440af828df3051
kevin zhang <kcrafterzh01@gmail.com>
2021-04-04 11:02:41 -0400

    Create create.js

---
+++ public/js/create.js
@@ -0,0 +1,17 @@
+document.getElementById("createB").onclick = createAccount;
+
+function createAccount(){
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let req = new XMLHttpRequest()
+    req.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 201){
+            console.log(this.responseText);
+            window.location.replace("http://localhost:3000/users/login");
+        }
+    }
+    req.open("POST", "http://localhost:3000/users");
+    req.setRequestHeader("Content-Type", "application/json");
+    req.send(JSON.stringify({"username": username, "password": password}));
+}
