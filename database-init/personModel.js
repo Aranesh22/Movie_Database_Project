67a85bbd36cdf5b539a2d0c5dc36aeded6a3c74a
Aranesh22 <a.aranesh@gmail.com>
2021-04-13 20:20:31 -0400

    Update personModel.js

--- database-init/personModel.js
+++ database-init/personModel.js
@@ -14,4 +14,14 @@ personSchema.query.byName = function(name){
     return this.where({name: new RegExp(name, "i")});
 }
 
+personSchema.query.byActor = function(act,dir) { 
+
+    return this.where("acted.0",{$exists: true}).where({name: new RegExp(act,"i")}).populate("acted directed");
+                //.where("directed.0",{$exists: true}).where({name: new RegExp(dir,"i")}).populate("acted"); 
+} 
+
+personSchema.query.byDirector = function(dir) { 
+
+    return this.where("directed.0",{$exists: true}).where({name:new RegExp(dir,"i")}).populate("acted directed");
+}
 module.exports = mongoose.model("Person", personSchema);
