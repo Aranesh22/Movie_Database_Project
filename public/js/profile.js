fff28627f868b1be18f96f344c6a3c90af3b9e63
kevin zhang <kcrafterzh01@gmail.com>
2021-04-16 13:56:17 -0400

    Update profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,8 @@
 document.getElementById("toggle").onclick = toggleAccount;
 document.getElementById("clear").onclick = clearNotifications;
+if(document.getElementById("unfollowPerson")) document.getElementById("unfollowPerson").onclick = unfollowPerson;
+if(document.getElementById("unfollowUser")) document.getElementById("unfollowUser").onclick = unfollowUser;
+if(document.getElementById("remove")) document.getElementById("remove").onclick = remove;
 
 function toggleAccount(){
     let req = new XMLHttpRequest();
@@ -22,5 +25,47 @@ function clearNotifications(){
         }
     }
     req.open("PUT", "http://localhost:3000/account/clearNotifications");
+    req.send();
+}
+
+function unfollowPerson(){
+    let link = document.getElementById("unfollowPerson").previousSibling.href;
+    console.log(link);
+    let req = new XMLHttpRequest();
+    req.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 204){
+            console.log(this.responseText);
+            location.reload();
+        }
+    }
+    req.open("PUT", link + "/unfollow");
+    req.send();
+}
+
+function unfollowUser(){
+    let link = document.getElementById("unfollowUser").previousSibling.href;
+    console.log(link);
+    let req = new XMLHttpRequest();
+    req.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 204){
+            console.log(this.responseText);
+            location.reload();
+        }
+    }
+    req.open("PUT", link + "/unfollow");
+    req.send();
+}
+
+function remove(){
+    let link = document.getElementById("remove").previousSibling.href;
+    console.log(link);
+    let req = new XMLHttpRequest();
+    req.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 204){
+            console.log(this.responseText);
+            location.reload();
+        }
+    }
+    req.open("PUT", link + "/removeWatchedMovie");
     req.send();
 }
